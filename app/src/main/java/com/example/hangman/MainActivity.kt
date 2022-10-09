7559cdb29ce7ca111c808993ce7ef98eccf13147
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-09 10:18:45 +0200

    display letters on button click

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.hangman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.util.Log
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rlayout : RelativeLayout
@@ -20,11 +21,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         init()
+        var indexplus = 0
+        var indexminus = alphabet.size - 1
+        var currentIndex : Int
+        var nextLetter : String
+
+        plusbutton.setOnClickListener{
+            if(indexplus < alphabet.size){
+                nextLetter = alphabet.elementAt(indexplus).toString()
+                letter.setText(nextLetter)
+                indexplus++
+            }
+            else if(indexplus == alphabet.size){
+                indexplus = 0
+            }
+            //TODO szín legyen fekete ha jól tippel
+        }
+
+        minusbutton.setOnClickListener{
+            if(indexminus > 0){
+                nextLetter = alphabet.elementAt(indexminus).toString()
+                letter.setText(nextLetter)
+                indexminus--
+            }
+            else if(indexminus == 0){
+                nextLetter = alphabet.elementAt(0).toString()
+                letter.setText(nextLetter)
+                indexminus = alphabet.size - 1
+            }
+        }
 
     }
 
     val wordArray = arrayOf("dekoracio", "webhely", "ezredes", "ejjeliszekreny", "fenyoillat", "kenyerpirito",
-    "madartoll", "aranyeso", "madartej", "dobvero")
+    "madartoll", "aranyeso", "madartej", "dobvero") //hard coded string array for game
+
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray() //alphabet for the buttons
 
     fun init(){
         rlayout = findViewById(R.id.rlayout)
